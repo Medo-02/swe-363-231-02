562f2de1b3e9a2e8a30fda549bd191dc89c3093a
Mohammed alzowaid <102298394+Medo-02@users.noreply.github.com>
2023-10-26 15:42:19 +0300

    JS 4 Exercise 2
    Remove old data structure and replace it with Objects, and classes.
    
    Remove old function and replace it with more effective functions.

--- exercises/game-ar.js
+++ exercises/game-ar.js
@@ -1,80 +1,93 @@
+//JS 4 Exercise 1 part 1
+class Game {
+  constructor(title, date, developer, devUrl, gameUrl, description){
+    this.title = title;
+    this.date = date;
+    this.developer = developer;
+    this.devUrl = devUrl;
+    this.gameUrl = gameUrl;
+    this.description = description;
+  }
+}
+//JS 4 Exercise 1 part 2
+const gameData = [
+  new Game(
+    "Resident Evil 7",
+    "January 24, 2017",
+    "Capcom",
+    "./Pictures/capcom.png",
+    "./Pictures/resident.jpeg",
+    "ي Resident Evil 7، يتلقى إيثان وينترز رسالة غامضة من زوجته ميا المفقودة منذ سنوات. يقوده هذا إلى مزرعة منعزلة في ريف لويزيانا، حيث يجد زوجته، ليكتشف أنها عنيفة وتتمتع بقدرات تجديدية غريبة."
+  ),
+  new Game(
+    "Bioshock Infinite",
+    "March 26, 2013",
+    "Irrational Games",
+    "./Pictures/irrational.jpeg",
+    "./Pictures/bioshock.jpeg",
+    "في عام 1912، تم اصطحاب بوكر ديويت بواسطة التوأم الغامضة لوتيس إلى منارة جزيرة قبالة ساحل ولاية ماين. بتعليمات من عائلة Luteces إحضار الفتاة لنا ومسح الدين، يدخل DeWitt المبنى الذي يضم صومعة الصواريخ التي تنقله إلى مدينة الطيران كولومبيا."
+  ),
+  new Game(
+    "The Witcher 3",
+    "May 18, 2015",
+    "CD Projekt Red",
+    "./Pictures/CD_Projekt_logo.svg.png",
+    "./Pictures/witcher 3.png",
+    "تجري اللعبة في عالم خيالي خيالي يعتمد على الأساطير السلافية. يتحكم اللاعبون في Geralt of Rivia، وهو قاتل وحوش مأجور يُعرف باسم Witcher، ويبحثون عن ابنته بالتبني، التي تهرب من Wild Hunt الآخر. يحارب اللاعبون مخاطر اللعبة العديدة بالأسلحة والسحر، ويتفاعلون مع شخصيات غير لاعبين، ويكملون المهام للحصول على نقاط الخبرة والذهب، والتي تُستخدم لزيادة قدرات Geralt وشراء المعدات. تحتوي قصة اللعبة على ثلاث نهايات محتملة، تحددها اختيارات اللاعب في النقاط الرئيسية في السرد."
+  ),
+  new Game(
+    "Fallout 4",
+    "November 10, 2015",
+    "Bethesda",
+    "./Pictures/bethesda.jpeg",
+    "./Pictures/fallout 4.jpeg",
+    "Fallout 4 هي لعبة تقمص أدوار في عالم مفتوح تم تطويرها بواسطة Bethesda Game Studios. في هذه اللعبة، يبدأ اللاعب كوالد وزوج في عام 2077، ولكن بعد ذلك يجب عليه دخول Vault 111 للنجاة من نهاية العالم النووية. يصبح اللاعب آخر الناجين المتبقين من Vault 111 عندما يخرج إلى الأرض القاحلة النووية في عام 2287، وسط أنقاض مدينة بوسطن."
+  )
+]
+//JS 4 Exercise 1 end
+
 // JS 2 Exercise 1
 
-const gamesData = 
-[
-    {
-      title: "Resident Evil 7",
-      date: "January 24, 2017",
-      developer: "Capcom",
-      devUrl: "./Pictures/capcom.png",
-      gameUrl: "./Pictures/resident.jpeg",
-      description: "ي Resident Evil 7، يتلقى إيثان وينترز رسالة غامضة من زوجته ميا المفقودة منذ سنوات. يقوده هذا إلى مزرعة منعزلة في ريف لويزيانا، حيث يجد زوجته، ليكتشف أنها عنيفة وتتمتع بقدرات تجديدية غريبة.",
-    },
-    {
-      title: "Bioshock Infinite",
-      date: "March 26, 2013",
-      developer: "Irrational Games",
-      devUrl: "./Pictures/irrational.jpeg",
-      gameUrl: "./Pictures/bioshock.jpeg",
-      description: "في عام 1912، تم اصطحاب بوكر ديويت بواسطة التوأم الغامضة لوتيس إلى منارة جزيرة قبالة ساحل ولاية ماين. بتعليمات من عائلة Luteces إحضار الفتاة لنا ومسح الدين، يدخل DeWitt المبنى الذي يضم صومعة الصواريخ التي تنقله إلى مدينة الطيران كولومبيا.",
-    },
-    {
-      title: "The Witcher 3",
-      date: "May 18, 2015",
-      developer: "CD Projekt Red",
-      devUrl: "./Pictures/CD_Projekt_logo.svg.png",
-      gameUrl: "./Pictures/witcher 3.png",
-      description: "تجري اللعبة في عالم خيالي خيالي يعتمد على الأساطير السلافية. يتحكم اللاعبون في Geralt of Rivia، وهو قاتل وحوش مأجور يُعرف باسم Witcher، ويبحثون عن ابنته بالتبني، التي تهرب من Wild Hunt الآخر. يحارب اللاعبون مخاطر اللعبة العديدة بالأسلحة والسحر، ويتفاعلون مع شخصيات غير لاعبين، ويكملون المهام للحصول على نقاط الخبرة والذهب، والتي تُستخدم لزيادة قدرات Geralt وشراء المعدات. تحتوي قصة اللعبة على ثلاث نهايات محتملة، تحددها اختيارات اللاعب في النقاط الرئيسية في السرد.",
-    },
-    {
-      title: "Fallout 4",
-      date: "November 10, 2015",
-      developer: "Bethesda",
-      devUrl: "./Pictures/bethesda.jpeg",
-      gameUrl: "./Pictures/fallout 4.jpeg",
-      description: "Fallout 4 هي لعبة تقمص أدوار في عالم مفتوح تم تطويرها بواسطة Bethesda Game Studios. في هذه اللعبة، يبدأ اللاعب كوالد وزوج في عام 2077، ولكن بعد ذلك يجب عليه دخول Vault 111 للنجاة من نهاية العالم النووية. يصبح اللاعب آخر الناجين المتبقين من Vault 111 عندما يخرج إلى الأرض القاحلة النووية في عام 2287، وسط أنقاض مدينة بوسطن.",
-    },
-  ];
 
-  function blogElement(gameData){
-    const container = document.createElement("div");
-    container.className = "blog-game";
-    const title = document.createElement("h1");
-    title.textContent = gamesData.title
-
-    const date = document.createElement("p");
-    date.className = "date"
-    date.textContent = gameData.date
-
-    const developerContainer = document.createElement("div");
-    developerContainer.className = "developer";
+  //JS 4 Exercise 2 part 1,2 nested function
+  const gameElement = (gameData) => {
+    //function to simplify the creation of elements and assigning classes
+    const createBlogElement = (tagType, className) => {
+      const element = document.createElement(tagType);
+      if (className) element.className = className;
+      return element;
+    };
     
-    const devImage = document.createElement("img")
-    devImage.className = "dev-img";
-    devImage.src = gameData.gameUrl;
 
-    const devDetail = document.createElement("div");
-    devDetail.className = "dev-detail";
-
-    const devName = document.createElement("p");
-    devName.className = "dev-name";
+    const container = createBlogElement("div", "blog-game");
+  
+    const title = createBlogElement("h1");
+    title.textContent = gameData.title;
+  
+    const date = createBlogElement("p", "date");
+    date.textContent = gameData.date;
+  
+    const developerContainer = createBlogElement("div", "developer");
+  
+    const devImage = createBlogElement("img", "dev-img");
+    devImage.src = gameData.devUrl;
+  
+    const devDetail = createBlogElement("div", "dev-detail");
+  
+    const devName = createBlogElement("p", "dev-name");
     devName.textContent = gameData.developer;
-
-    const gameImage = document.createElement("img")
-    gameImage.className = "game-img";
+  
+    const gameImage = createBlogElement("img", "game-img");
     gameImage.src = gameData.gameUrl;
-    
-    const gameDescription = document.createElement("p");
-    gameDescription.className = "game-desc";
+  
+    const gameDescription = createBlogElement("p", "game-desc");
     gameDescription.textContent = gameData.description;
-
-    const waterMarking = document.createElement("div");
-    waterMarking.className = "watermarking";
-
-    const watermark = document.createElement("div");
-    watermark.className = "watermark";
+  
+    const waterMarking = createBlogElement("div", "watermarking");
+  
+    const watermark = createBlogElement("div", "watermark");
     watermark.textContent = "MZ";
-
+  
     devDetail.appendChild(devName);
     devDetail.appendChild(date);
     developerContainer.appendChild(devImage);
@@ -88,14 +101,17 @@ const gamesData =
   
     return container;
   }
+  //JS 4 Exercise 2 nested function end
 
-  function addBlogGamesToWebsite() {
+  //JS 4 Exercise 2 part 1,3 creating arrow syntax and IIFE function
+  const addBlogGamesToWebsite = () => {
     const container = document.querySelector(".container-game");
   
     for (const gameData of gamesData) {
-      const blogGameElement = blogElement(gameData);
+      const blogGameElement = gameElement(gameData);
       container.appendChild(blogGameElement);
     }
   }
   
   addBlogGamesToWebsite();
+  //JS 4 Exercise 2 end
--- exercises/game-en.js
+++ exercises/game-en.js
@@ -1,4 +1,4 @@
-//JS 4 Exercise 1
+//JS 4 Exercise 1 part 1
 class Game {
   constructor(title, date, developer, devUrl, gameUrl, description){
     this.title = title;
@@ -9,7 +9,7 @@ class Game {
     this.description = description;
   }
 }
-
+//JS 4 Exercise 1 part 2
 const gameData = [
   new Game(
     "Resident Evil 7",
@@ -48,81 +48,46 @@ const gameData = [
 
 // JS 2 Exercise 1
 
-const gamesData = 
-[
-    {
-      title: "Resident Evil 7",
-      date: "January 24, 2017",
-      developer: "Capcom",
-      devUrl: "./Pictures/capcom.png",
-      gameUrl: "./Pictures/resident.jpeg",
-      description: "In Resident Evil 7, Ethan Winters receives a mysterious message from his wife Mia...",
-    },
-    {
-      title: "Bioshock Infinite",
-      date: "March 26, 2013",
-      developer: "Irrational Games",
-      devUrl: "./Pictures/irrational.jpeg",
-      gameUrl: "./Pictures/bioshock.jpeg",
-      description: "In 1912, Booker DeWitt is taken by the mysterious Lutece Twins to an island lighthouse...",
-    },
-    {
-      title: "The Witcher 3",
-      date: "May 18, 2015",
-      developer: "CD Projekt Red",
-      devUrl: "./Pictures/CD_Projekt_logo.svg.png",
-      gameUrl: "./Pictures/witcher 3.png",
-      description: "The game takes place in a fictional fantasy world based on Slavic mythology...",
-    },
-    {
-      title: "Fallout 4",
-      date: "November 10, 2015",
-      developer: "Bethesda",
-      devUrl: "./Pictures/bethesda.jpeg",
-      gameUrl: "./Pictures/fallout 4.jpeg",
-      description: "Fallout 4 is an open-world role-playing game developed by Bethesda Game Studios...",
-    },
-  ];
 
-  function blogElement(gameData){
-    const container = document.createElement("div");
-    container.className = "blog-game";
-    const title = document.createElement("h1");
-    title.textContent = gamesData.title
-
-    const date = document.createElement("p");
-    date.className = "date"
-    date.textContent = gameData.date
-
-    const developerContainer = document.createElement("div");
-    developerContainer.className = "developer";
+  //JS 4 Exercise 2 part 1,2 nested function
+  const gameElement = (gameData) => {
+    //function to simplify the creation of elements and assigning classes
+    const createBlogElement = (tagType, className) => {
+      const element = document.createElement(tagType);
+      if (className) element.className = className;
+      return element;
+    };
     
-    const devImage = document.createElement("img")
-    devImage.className = "dev-img";
-    devImage.src = gameData.gameUrl;
 
-    const devDetail = document.createElement("div");
-    devDetail.className = "dev-detail";
-
-    const devName = document.createElement("p");
-    devName.className = "dev-name";
+    const container = createBlogElement("div", "blog-game");
+  
+    const title = createBlogElement("h1");
+    title.textContent = gameData.title;
+  
+    const date = createBlogElement("p", "date");
+    date.textContent = gameData.date;
+  
+    const developerContainer = createBlogElement("div", "developer");
+  
+    const devImage = createBlogElement("img", "dev-img");
+    devImage.src = gameData.devUrl;
+  
+    const devDetail = createBlogElement("div", "dev-detail");
+  
+    const devName = createBlogElement("p", "dev-name");
     devName.textContent = gameData.developer;
-
-    const gameImage = document.createElement("img")
-    gameImage.className = "game-img";
+  
+    const gameImage = createBlogElement("img", "game-img");
     gameImage.src = gameData.gameUrl;
-    
-    const gameDescription = document.createElement("p");
-    gameDescription.className = "game-desc";
+  
+    const gameDescription = createBlogElement("p", "game-desc");
     gameDescription.textContent = gameData.description;
-
-    const waterMarking = document.createElement("div");
-    waterMarking.className = "watermarking";
-
-    const watermark = document.createElement("div");
-    watermark.className = "watermark";
+  
+    const waterMarking = createBlogElement("div", "watermarking");
+  
+    const watermark = createBlogElement("div", "watermark");
     watermark.textContent = "MZ";
-
+  
     devDetail.appendChild(devName);
     devDetail.appendChild(date);
     developerContainer.appendChild(devImage);
@@ -136,14 +101,17 @@ const gamesData =
   
     return container;
   }
+  //JS 4 Exercise 2 nested function end
 
-  function addBlogGamesToWebsite() {
+  //JS 4 Exercise 2 part 1,3 creating arrow syntax and IIFE function
+  const addBlogGamesToWebsite = () => {
     const container = document.querySelector(".container-game");
   
     for (const gameData of gamesData) {
-      const blogGameElement = blogElement(gameData);
+      const blogGameElement = gameElement(gameData);
       container.appendChild(blogGameElement);
     }
   }
   
   addBlogGamesToWebsite();
+  //JS 4 Exercise 2 end
